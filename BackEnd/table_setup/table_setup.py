757f719ca4dfd16e0109629fb3e74bccdae97cfd
ZRQ-rikkie <Z986929216@gmail.com>
2023-06-29 23:11:14 +0100

    setup basic django and link the RDS postgres. setup user and poi table using django ORM

--- BackEnd/table_setup/table_setup.py
+++ BackEnd/table_setup/table_setup.py
@@ -6,11 +6,11 @@ cursor = None
 try:
     # Establish a connection to the database
     connection = psycopg2.connect(
-        user="postgres",
-        password="******",
-        host="localhost",
-        port="5432",
-        database="Summer_project"
+        host="newyorkbusy.cmbwzu1bhkey.us-east-1.rds.amazonaws.com",  
+        dbname="newyorkbusy", 
+        user="DB4COMP47360",
+        password="researchpracticum",  
+        port="5432"  
     )
 
     cursor = connection.cursor()
@@ -22,7 +22,9 @@ try:
         Username VARCHAR(255) UNIQUE NOT NULL,
         Password VARCHAR(255) NOT NULL,
         Email VARCHAR(255) UNIQUE NOT NULL,
-        Preference JSONB
+        Category1 VARCHAR(255) NOT NULL,
+        Category2 VARCHAR(255) NOT NULL,
+        Category3 VARCHAR(255) NOT NULL
     );
     """
 
@@ -32,33 +34,32 @@ try:
         POIName VARCHAR(255) NOT NULL,
         Description TEXT,
         Latitude FLOAT NOT NULL,
-        Longitude FLOAT NOT NULL
+        Longitude FLOAT NOT NULL,
+        OpenningHours FLOAT NOT NULL,
+        Category VARCHAR(255) NOT NULL
+    
     );
     """
 
-    create_userpois_table = """
-    CREATE TABLE UserPOIs (
-        UserPOIID SERIAL PRIMARY KEY,
+
+
+    create_userbucketlist_table = """
+    CREATE TABLE UserBucketlist (
+        BucketlistID SERIAL PRIMARY KEY,
         UserID INT REFERENCES Users(UserID),
         POIID INT REFERENCES POIs(POIID),
         StartDate DATE,
         EndDate DATE
+        
     );
     """
 
-    create_bucketlist_table = """
-    CREATE TABLE Bucketlist (
-        BucketlistID SERIAL PRIMARY KEY,
-        UserID INT REFERENCES Users(UserID),
-        POIID INT REFERENCES POIs(POIID)
-    );
-    """
+
 
     # Execute the CREATE TABLE statements
     cursor.execute(create_users_table)
     cursor.execute(create_pois_table)
-    cursor.execute(create_userpois_table)
-    cursor.execute(create_bucketlist_table)
+    cursor.execute(create_userbucketlist_table)
 
     # Commit the transaction
     connection.commit()
