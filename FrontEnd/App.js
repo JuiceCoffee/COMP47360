657872dd9c8881acaf41a020165e5eb3522774fe
yuan <yingyuan.li@ucdconnect.ie>
2023-06-30 10:54:24 +0100

    using POI api
    the code works on mobile, but tailwindcss does not  work on web.
    POI used the tripadvisor api.

---
+++ FrontEnd/App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaView, Text, View, Platform } from "react-native";
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "./screens/HomeScreen";
+import Discover from "./screens/Discover";
+import ItemScreen from "./screens/ItemScreen";
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  const isWeb = Platform.OS === "web";
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Discover" component={Discover} />
+        <Stack.Screen name="ItemScreen" component={ItemScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
